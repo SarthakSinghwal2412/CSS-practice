f0ca720782458b08fb88a2d23a3b87b440870628
SarthakSinghwal2412 <sarthaksinghwal2412@gmail.com>
2022-04-29 23:26:09 +0530

    commit

--- src/components/mainComp/MainComp.jsx
+++ src/components/mainComp/MainComp.jsx
@@ -1,11 +1,31 @@
 import { Box } from '@mui/material'
 import React from 'react';
 import Divider from '@mui/material/Divider';
-
+import './mainComp.css';
+import NetworkWifiIcon from '@mui/icons-material/NetworkWifi';
+import  CalendarViewMonthIcon from '@mui/icons-material/CalendarViewMonth';
 function MainComp() {
   return (
     <Box bgcolor = "white" flex = {4} p = {1}>
-        <Divider className = "divider"/>
+        <div className='buttons'>
+            <div className = "permission">
+                <NetworkWifiIcon/>
+               <span>Permissions</span>
+            </div>
+            <div className="approval">
+              <CalendarViewMonthIcon/>
+              <span>Approval</span>
+            </div>
+            <div className = 'vertical'>
+
+            </div>
+            
+        </div>
+        <div className='line'>
+            <Divider/>
+        </div>
+        
+        
     </Box>
   )
 }
